b61f74d1fff45ffd5a9d8625f8634efe07333322
Eshrak <66720486+esrakcodes@users.noreply.github.com>
2023-12-23 13:42:03 +0600

    Updated script.js

--- script.js
+++ script.js
@@ -1,13 +1,81 @@
+class Node {
+  constructor(score) {
+    this.score = score;
+    this.next = null;
+  }
+}
+
+class HighScoreLinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addScore(score) {
+    const newNode = new Node(score);
+    if (!this.head || score > this.head.score) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next && score <= current.next.score) {
+        current = current.next;
+      }
+      newNode.next = current.next;
+      current.next = newNode;
+    }
+  }
+
+  removeScore() {
+    if (!this.head) return;
+    this.head = this.head.next;
+  }
+
+  getHighScore() {
+    return this.head ? this.head.score : 0;
+  }
+}
+
+class SpeedLinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+    this.multiplier = 100;
+  }
+
+  addSpeedIncrease(value) {
+    const newNode = { value };
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.size++;
+  }
+
+  removeSpeedIncrease() {
+    if (!this.head) return;
+    this.head = this.head.next;
+    this.size--;
+  }
+
+  getIntervalDuration() {
+    return this.size > 0 ? this.multiplier / this.size : this.multiplier;
+  }
+}
+
 let board = document.querySelector('#board');
 let scoreElement = document.querySelector('#score');
 let highScoreElement = document.querySelector('#highScore');
+let restartPopup = document.getElementById('restartPopup');
+let restartButton = document.getElementById('restartButton');
 
 let snake = {};
 let apple;
 let score;
 let updateInterval;
 let SQUARE_SIZE = 4;
-let ROWS = COLUMNS = 100/SQUARE_SIZE;
+let ROWS = COLUMNS = 100 / SQUARE_SIZE;
 let KEY = {
   LEFT: 37,
   UP: 38,
@@ -16,6 +84,9 @@ let KEY = {
 };
 let xDown = null;
 let yDown = null;
+
+let highScores = new HighScoreLinkedList();
+let speedList = new SpeedLinkedList();
 
 document.querySelector('body').addEventListener('keydown', setNextDirection);
 document.addEventListener('touchstart', handleTouchStart);
@@ -26,13 +97,13 @@ alert("Use your keyboard or swipe to control the snake.");
 
 function init() {
   snake.body = [];
-  snake.head = makeSnakeSquare(10, 10)
+  snake.head = makeSnakeSquare(10, 10);
   snake.head.setAttribute('id', 'snake-head');
   apple = makeApple();
   scoreElement.innerHTML = "Score: 0";
   score = 0;
   calculateAndDisplayHighScore();
-  updateInterval = setInterval(update, 100);
+  updateInterval = setInterval(update, speedList.getIntervalDuration());
 }
 
 function update() {
@@ -56,10 +127,15 @@ function moveSnake() {
   }
 
   snake.head.direction = snake.head.nextDirection;
-  if (snake.head.direction === "left") { snake.head.column--; }
-  else if (snake.head.direction === "right") { snake.head.column++; }
-  else if (snake.head.direction === "up") { snake.head.row--; }
-  else if (snake.head.direction === "down") { snake.head.row++; }
+  if (snake.head.direction === "left") {
+    snake.head.column--;
+  } else if (snake.head.direction === "right") {
+    snake.head.column++;
+  } else if (snake.head.direction === "up") {
+    snake.head.row--;
+  } else if (snake.head.direction === "down") {
+    snake.head.row++;
+  }
 
   repositionSquare(snake.head, snake.head.row, snake.head.column);
 }
@@ -75,11 +151,20 @@ function handleAppleCollision() {
   makeApple();
   let row = snake.tail.row;
   let column = snake.tail.column;
-  if (snake.tail.direction === "left") { column++; }
-  else if (snake.tail.direction === "right") { column--; }
-  else if (snake.tail.direction === "up") { row++; }
-  else if (snake.tail.direction === "down") { row--; }
+  if (snake.tail.direction === "left") {
+    column++;
+  } else if (snake.tail.direction === "right") {
+    column--;
+  } else if (snake.tail.direction === "up") {
+    row++;
+  } else if (snake.tail.direction === "down") {
+    row--;
+  }
   makeSnakeSquare(row, column);
+
+  if (score === 4) {
+    speedUp();
+  }
 }
 
 function hasCollidedWithSnake() {
@@ -98,7 +183,9 @@ function endGame() {
   clearInterval(updateInterval);
   removeAllChildElements(board);
   calculateAndDisplayHighScore();
-  setTimeout(function() { init(); }, 500);
+  setTimeout(function () {
+    init();
+  }, 500);
 }
 
 function removeAllChildElements(parent) {
@@ -120,8 +207,8 @@ function makeSnakeSquare(row, column) {
 function repositionSquare(square, row, column) {
   square.row = row;
   square.column = column;
-  const leftOffset = (column-1) * SQUARE_SIZE + "%";
-  const topOffset = (row-1) * SQUARE_SIZE + "%";
+  const leftOffset = (column - 1) * SQUARE_SIZE + "%";
+  const topOffset = (row - 1) * SQUARE_SIZE + "%";
   square.style.left = leftOffset;
   square.style.top = topOffset;
 }
@@ -155,48 +242,56 @@ function getRandomAvailablePosition() {
 function setNextDirection(event) {
   let keyPressed = event.which;
   if (snake.head.direction !== "left" && snake.head.direction !== "right") {
-    if (keyPressed === KEY.LEFT) { snake.head.nextDirection = "left"; }
-    if (keyPressed === KEY.RIGHT) { snake.head.nextDirection = "right"; }
+    if (keyPressed === KEY.LEFT) {
+      snake.head.nextDirection = "left";
+    }
+    if (keyPressed === KEY.RIGHT) {
+      snake.head.nextDirection = "right";
+    }
   }
   if (snake.head.direction !== "up" && snake.head.direction !== "down") {
-    if (keyPressed === KEY.UP) { snake.head.nextDirection = "up"; }
-    if (keyPressed === KEY.DOWN) { snake.head.nextDirection = "down"; }
+    if (keyPressed === KEY.UP) {
+      snake.head.nextDirection = "up";
+    }
+    if (keyPressed === KEY.DOWN) {
+      snake.head.nextDirection = "down";
+    }
   }
 }
 
 function calculateAndDisplayHighScore() {
-  let highScore = sessionStorage.getItem("highScore") || 0;
-  if (score > highScore) {
-    sessionStorage.setItem("highScore", score);
-    highScore = score;
-    alert("New High Score!");
-  }
-  highScoreElement.innerHTML = "High Score: " + highScore;
+  highScores.addScore(score);
+  highScoreElement.textContent = "High Score: " + highScores.getHighScore();
+}
+
+function speedUp() {
+  speedList.addSpeedIncrease(5);
+  clearInterval(updateInterval);
+  updateInterval = setInterval(update, speedList.getIntervalDuration());
 }
 
 function handleTouchStart(evt) {
-    xDown = evt.touches[0].clientX;
-    yDown = evt.touches[0].clientY;
+  xDown = evt.touches[0].clientX;
+  yDown = evt.touches[0].clientY;
 }
 
 function handleTouchMove(evt) {
-    if (!xDown || !yDown) {
-        return;
+  if (!xDown || !yDown) {
+    return;
+  }
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    if (snake.head.direction !== "left" && snake.head.direction !== "right") {
+      snake.head.nextDirection = xDiff > 0 ? "left" : "right";
     }
-    var xUp = evt.touches[0].clientX;
-    var yUp = evt.touches[0].clientY;
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-    if (Math.abs(xDiff) > Math.abs(yDiff)) {
-      if (snake.head.direction !== "left" && snake.head.direction !== "right") {
-        snake.head.nextDirection = xDiff > 0 ? "left" : "right";
-      }
+  } else {
+    if (snake.head.direction !== "up" && snake.head.direction !== "down") {
+      snake.head.nextDirection = yDiff > 0 ? "up" : "down";
     }
-    else {
-      if (snake.head.direction !== "up" && snake.head.direction !== "down") {
-        snake.head.nextDirection = yDiff > 0 ? "up" : "down";
-      }
-    }
-    xDown = null;
-    yDown = null;
+  }
+  xDown = null;
+  yDown = null;
 }
